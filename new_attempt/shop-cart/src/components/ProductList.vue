84d6b488ab331d440e6fb1b1ff4f82db95d7a21f
Pushkaraj <pushkaraj.sane99@gmail.com>
2022-10-10 16:46:54 +0530

    newatmpt

---
+++ new_attempt/shop-cart/src/components/ProductList.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+            <h1>Prod List</h1>
+
+            <ul>
+              <li v-for="(product,x) in products" :key="x">{{product.title}} - {{product.price}}</li>
+
+
+            </ul>
+            
+    </div>
+
+
+
+</template>
+
+
+<script>
+   
+   import shop from '@/api/shop.js'
+
+   export default{
+      name : "ProductList",
+      data(){
+
+        return {
+
+           products:[]
+
+
+
+        }
+
+      } ,
+
+      created() {
+
+          shop.getProducts(products=> {
+
+             this.products=products
+
+
+          })
+         
+
+      }
+
+
+   }
+   
+
+</script>
+
+
+<style>
+
+
+
+</style>
+
